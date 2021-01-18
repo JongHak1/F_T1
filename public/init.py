5fbe248141562a53f97e4cef5039da94bd24eca6
JongHak1 <jh927701@naver.com>
2021-01-18 06:20:05 +0000

    Main Server

---
+++ public/init.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World'
+
+app.run(host='0.0.0.0', port=8100, threaded = True)
